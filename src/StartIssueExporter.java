6ce5cb6a0c69839dda1c7110f32db6f34bde5c1b
Markus Krainz <ldm@gmx.at>
2011-07-25 23:54:19 +0200

    cleanup + javadoc

--- src/StartIssueExporter.java
+++ src/StartIssueExporter.java
@@ -15,6 +15,17 @@ import java.util.zip.GZIPInputStream;
 import org.codehaus.jackson.*;
 import org.codehaus.jackson.map.*;
 
+
+/**
+ * 
+ * Exports issue data from GitHub as CSV.
+ * Uses the (somewhat retarded) http://developer.github.com/v3/issues/ interface to do so.
+ * 
+ * Warning: Basic Auth is used (with HTTPS).
+ * 
+ * @author Markus Krainz
+ *
+ */
 public class StartIssueExporter {
 
     // input
@@ -31,29 +42,24 @@ public class StartIssueExporter {
     // output
     public static final char CSV = ';';
     public static final String OUT_FILE = "/media/tmpfs/lol/" + repo + ".csv";
-    
 
     public static void main(final String... args) {
-
-	
-	System.err.println(CONST_URI);
-	System.err.flush();
-	
-	
 	try {
 	    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(OUT_FILE)));
-//	    PrintWriter out = new PrintWriter(
-//		    new BufferedWriter(new OutputStreamWriter(System.out)));
 
+	    //write comumn headers
+	    //see writeCSV() for row generation
 	    out.append("number" + CSV + "title" + CSV + "state" + CSV + "created" + CSV + "labels"
 		    + CSV + "comments" + '\n');
 	    out.flush();
 
+	    //write open issues
 	    int pages = getNumPages(false);
 	    for (int page = 1; page <= pages; page++) {
 		process(page, false, out);
 	    }
-	    
+
+	    //write closed issues
 	    pages = getNumPages(true);
 	    for (int page = 1; page <= pages; page++) {
 		process(page, true, out);
@@ -65,12 +71,19 @@ public class StartIssueExporter {
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
-	
+
     }
 
     /**
+     * Queries the number of pages from GitHub.
+     * Uses the "Link" HTTP header set by GitHub as "last" to do so. 
      * 
-     * @return number of pages
+     * @param closed
+     *            true if you want the closed issues, false if you want the open
+     *            issues
+     * @return the number of pages
+     * @throws RuntimeException
+     *             if an (IO) Error occurred
      */
     static int getNumPages(boolean closed) {
 	HttpURLConnection urlc = null;
@@ -81,16 +94,17 @@ public class StartIssueExporter {
 	try {
 	    int response = 0;
 
-	    urlc = (HttpURLConnection) (new URL(CONST_URI+"?state="+(closed?"closed":"open")).openConnection());
+	    urlc = (HttpURLConnection) (new URL(CONST_URI + "?state="
+		    + (closed ? "closed" : "open")).openConnection());
 	    urlc.setRequestMethod("HEAD");
 	    urlc.setRequestProperty("Authorization", "Basic " + AUTHENTICATION);
 	    urlc.connect();
 
 	    Map<String, List<String>> headerFields = urlc.getHeaderFields();
 	    List<String> list = headerFields.get("Link");
-	    
-	    //if there is no Link header assume there is only one page.
-	    if (list ==null || list.size() == 0)
+
+	    // if there is no Link header assume there is only one page.
+	    if (list == null || list.size() == 0)
 		return 1;
 
 	    response = urlc.getResponseCode();
@@ -101,12 +115,12 @@ public class StartIssueExporter {
 
 	    System.err.println(lastLink);
 
-	    //get total number of pages from link to last page
+	    // get total number of pages from link to last page
 	    pages = Integer.parseInt(lastLink.substring(lastLink.lastIndexOf('?') + 6,
 		    lastLink.lastIndexOf('&')));
 
 	} catch (IOException e) {
-	    throw new RuntimeException("An error occured while reading number of pages.",e);
+	    throw new RuntimeException("An error occured while reading number of pages.", e);
 	} finally {
 	    if (is != null)
 		try {
@@ -115,23 +129,41 @@ public class StartIssueExporter {
 		    e.printStackTrace();
 		}
 	}
-	
-	//github sometimes returns 0 if there is really 1 page
-	pages=pages==0?1:pages;
+
+	// github sometimes returns 0 if there is really 1 page
+	pages = pages == 0 ? 1 : pages;
 
 	return pages;
     }
 
-    static void process(int page, boolean closed, PrintWriter out) throws JsonParseException, IOException {
+    /**
+     * Dumps issue data from specified page to a PrintWriter.
+     * 
+     * @param page
+     *            pagenumber you want to query (begin with 1, and use
+     *            getNumPages() for max page number)
+     * @param closed
+     *            true if you want the closed issues, false if you want the open
+     *            issues
+     * @param out
+     *            Printwriter you would like to dump the data to
+     * @throws JsonParseException
+     *             if there was an Exception while parsing the JSON data from
+     *             github
+     * @throws IOException
+     *             if there was an IO Exception while loading the data over HTTP
+     */
+    static void process(int page, boolean closed, PrintWriter out) throws JsonParseException,
+	    IOException {
 
 	HttpURLConnection urlc = null;
 	InputStream is = null;
 
 	try {
-
 	    int response = 0;
 
-	    urlc = (HttpURLConnection) (new URL(CONST_URI +"?state="+(closed?"closed":"open") +"&page=" + page).openConnection());
+	    urlc = (HttpURLConnection) (new URL(CONST_URI + "?state="
+		    + (closed ? "closed" : "open") + "&page=" + page).openConnection());
 
 	    urlc.setRequestProperty("Authorization", "Basic " + AUTHENTICATION);
 	    urlc.setRequestProperty("Accept-Encoding", "gzip");
@@ -155,12 +187,6 @@ public class StartIssueExporter {
 		is = new GZIPInputStream(is);
 	    }
 
-	    // BufferedReader br = new BufferedReader(new
-	    // InputStreamReader(is));
-	    // while (br.ready()) {
-	    // System.out.println(br.readLine());
-	    // }
-
 	    writeCSV(is, out);
 
 	} catch (IOException e) {
@@ -177,6 +203,19 @@ public class StartIssueExporter {
 	}
     }
 
+    /**
+     * Dumps issue data formated as JSON from specified InputStream to a Printwriter as CSV.
+     * 
+     * @param is InputStream we read JSON from
+     * @param out
+     *            Printwriter you would like to dump the data to
+     * @throws JsonParseException
+     *             if there was an Exception while parsing the JSON data from
+     *             github
+     * @throws JsonMappingException if there was an error mapping JSON to JsonNodes
+     * @throws IOException
+     *             if there was an IO Exception while loading the data over HTTP
+     */
     public static void writeCSV(InputStream is, PrintWriter out) throws JsonParseException,
 	    JsonMappingException, IOException {
 
@@ -198,7 +237,9 @@ public class StartIssueExporter {
 		if (labels.hasNext())
 		    labelString += '+';
 	    }
-
+	    
+	    
+	    //see main() for column headers
 	    out.append(url.subSequence(url.lastIndexOf('/') + 1, url.length()).toString() + CSV
 		    + issue.get("title").getTextValue() + CSV + issue.get("state").getTextValue()
 		    + CSV + issue.get("created_at").getTextValue() + CSV + labelString + CSV
@@ -207,47 +248,4 @@ public class StartIssueExporter {
 	}
 
     }
-
-    @Deprecated
-    static void processStream() throws JsonParseException, IOException {
-	JsonFactory f = new JsonFactory();
-	JsonParser jp = f.createJsonParser(new File("/media/tmpfs/measurement.txt"));
-
-	// PrintWriter out = new PrintWriter(new BufferedWriter(new
-	// FileWriter("/media/tmpfs/foo.csv")));
-	PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-
-	jp.nextToken();
-
-	while (jp.hasCurrentToken()) {
-
-	    String created = null, state = null, number = null, labels = null, comments = null, title = null;
-
-	    String fieldname = jp.getCurrentName();
-	    jp.nextToken();
-
-	    if ("created_at".equals(fieldname)) {
-		String url = jp.getCurrentName();
-		String value = jp.getText();
-
-	    } else if ("state".equals(fieldname)) {
-
-	    } else if ("url".equals(fieldname)) {
-		number = jp.getText();
-
-	    } else if ("labels".equals(fieldname)) {
-
-	    } else if ("comments".equals(fieldname)) {
-
-		comments = jp.getText();
-
-		out.append(number + ',' + title + ',' + state + ',' + created + ',' + labels + ','
-			+ comments + '\n');
-	    }
-
-	    out.flush();
-	}
-
-    }
-
 }
